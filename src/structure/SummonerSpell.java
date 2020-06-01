1391c2d0430ecc32be272d30e8f7b99b9c48ff34
Thierry <fofi15cd@gmail.com>
2020-06-01 17:07:10 -0400

    changed SummonerSpells to SummonerSpell, enum to class

---
+++ src/structure/SummonerSpell.java
@@ -0,0 +1,87 @@
+package structure;
+
+public class SummonerSpell
+{
+	/**
+	 * The base path of all the summoner spells' images' path
+	 */
+	private static final String PATH = "image\\summoner spells\\";
+
+	/**
+	 * The name of the summoner spell
+	 */
+	private String name;
+
+	/**
+	 * The list of Map in which you can take the summoner spell
+	 */
+	private Map[] usableInMaps;
+
+	/**
+	 * The name of the image of the summoner spell
+	 */
+	private String imgName;
+
+	/**
+	 * The complete path of the image of the summoner spell
+	 */
+	private String imgPath;
+
+	/**
+	 * Creates a new summoner spell
+	 * 
+	 * @param name : name of the summoner spell
+	 * @param usableInMap : a list of Map in which you can take the summoner
+	 *            spell
+	 */
+	private SummonerSpell(String name, Map[] usableInMap)
+	{
+		this.name = name;
+		this.usableInMaps = usableInMap;
+
+		this.imgName = getName() + ".png";
+		this.imgPath = PATH + imgName;
+	}
+
+	/**
+	 * Returns the name of the summoner spell
+	 * 
+	 * @return the name of the summoner spell
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+
+	/**
+	 * Returns the Map in which you can take the summoner spell
+	 * 
+	 * @return the Map in which you can take the summoner spell
+	 */
+	public Map[] usableInMap()
+	{
+		return usableInMaps;
+	}
+
+	/**
+	 * Returns the path of the image of the summoner spell
+	 * 
+	 * @return the path of the image of the summoner spell
+	 */
+	public String getImgPath()
+	{
+		return imgPath;
+	}
+
+	/**
+	 * Returns the name of the summoner spell
+	 * 
+	 * @return the name of the summoner spell
+	 */
+	@Override
+	public String toString()
+	{
+		return getName();
+	}
+
+}
